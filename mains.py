1a93e2ff478b9a30ed49707383b9b7f827a722f9
dillPickle2 <95428652+dillPickle2@users.noreply.github.com>
2021-12-02 21:42:34 +0530

    Update mains.py

--- mains.py
+++ mains.py
@@ -3,12 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return render_template("home.html")
+def index():
+    # Load current count
+    f = open("count.txt", "r")
+    count = int(f.read())
+    f.close()
+    # Increment the count
+    count += 1
+    # Overwrite the count
+    f = open("count.txt", "w")
+    f.write(str(count))
+    f.close()
     
-@app.route("/about")
-def about():
-    return render_template("about.html")
-    
+    return render_template("index.html", count=count)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
